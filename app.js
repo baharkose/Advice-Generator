bf0b4dbef7af5559029d25cb6894bf353e56808e
baharkose <baharkse17@gmail.com>
2023-10-23 19:28:42 +0300

    created html css and js

---
+++ app.js
@@ -0,0 +1,33 @@
+const btn = document.querySelector(".btn");
+const spanEl = document.querySelector(".id");
+const adviceEl = document.querySelector(".advice");
+
+// API verilerini saklamak için bir değişken
+let apiData = null;
+
+// API'yi çağıran işlev
+async function fetchData() {
+    try {
+        const response = await fetch("https://api.adviceslip.com/advice");
+        if (!response.ok) {
+            throw new Error("HTTP hata: " + response.status);
+        }
+        const data = await response.json();
+        console.log(data.slip.advice);
+        const advice = data.slip.advice;
+        const idA = data.slip.id;
+
+        adviceEl.innerHTML = advice;
+        spanEl.innerHTML = idA;
+
+        // API verilerini sakla
+        apiData = data;
+    } catch (error) {
+        console.error("Bir hata oluştu:", error);
+    }
+}
+
+// Sayfa yüklendiğinde ve düğmeye tıklandığında API'yi çağır
+window.onload = fetchData;
+
+btn.addEventListener("click", fetchData);
